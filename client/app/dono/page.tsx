5cc2ab934731b99af2bbc4f261889aff47cd2a9d
Thiago Borges <thandbor@gmail.com>
2025-09-19 17:54:50 -0300

    fix: get current account

--- client/app/dono/page.tsx
+++ client/app/dono/page.tsx
@@ -4,6 +4,8 @@ import { useCurrentAccount, ConnectButton } from "@mysten/dapp-kit";
 
 
 export default function Home(){
+
+    const currentAccount = useCurrentAccount();
     return (
     <div className="flex flex-col items-center gap-2">
         <div className = "w-full flex justify-end items-center p-4 h-20">
