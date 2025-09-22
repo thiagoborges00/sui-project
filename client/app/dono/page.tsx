723489c542a7362f2c4065771394aaa826873f9e
Thiago Borges <thandbor@gmail.com>
2025-09-22 09:31:03 -0300

    update front and install walrus

--- client/app/dono/page.tsx
+++ client/app/dono/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useCurrentAccount, ConnectButton } from "@mysten/dapp-kit";
+import {useGalleryData} from "@/hooks/useGalleryData";
 
 
 export default function Home(){
 
     const currentAccount = useCurrentAccount();
+    const gallery  = useGalleryData();
     return (
     <div className="flex flex-col items-center gap-2">
         <div className = "w-full flex justify-end items-center p-4 h-20">
--- client/hooks/useGalleryData.tsx
+++ client/hooks/useGalleryData.tsx
@@ -1,9 +1,25 @@
-import { useSuiClient } from "@mysten/dapp-kit"
+import { useCurrentAccount, useSuiClient } from "@mysten/dapp-kit"
 
 export const useGalleryData = () => {
 
-    const client = useSuiClient();
-    // parou no minuto 43
-    //https://www.youtube.com/watch?v=d_XiWK64qGg
+    const currentAccount = useCurrentAccount();
 
-} 
+    const client = useSuiClient();
+    
+    const galleryData = async ()=>{
+        if (!currentAccount) return [];
+        const gallery: any = await client.getObject({
+            id:'0xada4e717c62645d51b4cca57b11d9f1bf343b595143c027ab6afbcaec3199c05',
+            options: {
+                showContent: true,
+                showOwner: true,
+            }
+        });
+
+        return { ...gallery.data.content.fields};
+    };
+    return {
+        galleryData
+    };
+
+};
